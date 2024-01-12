23c3a9ce354dcda356fec2203d9d1c5b6a18d85f
Ngan LE <ngan.ltkn2911@gmail.com>
2024-01-12 17:38:05 +0100

    added _base scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import logo from "./logo.svg";
-import "./App.css";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
-import HeaderFlix from "./components/HeaderFlix/HeaderFlix";
 import DettailPage from "./Pages/DetailPage/DettailPage";
-import FooterFlix from "./components/FooterFlix/FooterFlix";
 import Layout from "./Layout/Layout";
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import "./assets/scss/index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
